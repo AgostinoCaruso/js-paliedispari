f3849d68412966a773dd0ae616ec1f27c76ef221
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-05 17:37:25 +0100

    add menu to switch game

--- script/script.js
+++ script/script.js
@@ -6,7 +6,6 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
-//PalindromoGioco();
 /*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -15,13 +14,26 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
-PariDispariGioco();
-
+MenuScelta();
 //function-----------------------------------------------------
+function MenuScelta(){
+    let sceltaMenu = parseInt(prompt("Scegli:\n1) Per palindromo\n2) Per pari o dispari"));
+    switch(sceltaMenu){
+        case 1:
+            PalindromoGioco();
+            break;
+        case 2:
+            PariDispariGioco();
+            break;
+        default:
+            console.log("Hai inserito un valore non corretto");
+            break;
+    }
+}
 //Palindromo
 function PalindromoGioco() {
     //Chiedo all'utente una parola
-    const parolaUtente = prompt("Scrivi una parola per verificare se è palindroma!");
+    const parolaUtente = prompt("Scrivi una parola per verificare se è palindroma!").toLowerCase();
     let parolaGirata = "";//Inizializzo vuota una stringa
 
     if (isNaN(parolaUtente)) {//verifico se l'utente abbia inserito una parola
@@ -36,25 +48,37 @@ function PalindromoGioco() {
     }
 }
 //pari dispari
-function PariDispariGioco(){
+function PariDispariGioco() {
     let contatore = 0;
     let disparoParoScelta = prompt("Scegli tra dispari e pari").toLowerCase();//aspetto user input
-    while(disparoParoScelta != "pari" || disparoParoScelta != "dispari"){
+    while (disparoParoScelta != "pari" && disparoParoScelta != "dispari" && contatore <= 3) {
+        console.log(contatore);
         disparoParoScelta = prompt("Hai inserito una parola non valida, pari o dispari").toLowerCase();//aspetto user input
         console.log(disparoParoScelta);
         contatore++;
     }
+    contatore = 0;
     let numeroScelto = parseInt(prompt("Digita un numero tra 1 a 5"));//aspetto user input
-    let numeroPc = GeneraNumero(5,1);//genero il numero
-    let somma = numeroScelto + numeroPc;
-    isNumberEven(somma);//controlla se il numero è paro o disparo
-    if(disparoParoScelta == "pari" && isNumberEven(somma)){
-        console.log("hai vinto, hai scelto pari (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
-    }else if(disparoParoScelta == "dispari" && !isNumberEven(somma)){
-        console.log("hai vinto, hai scelto dispari (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
-    }else{
-        console.log("non hai vinto, hai scelto " + disparoParoScelta + " (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
+    while ((numeroScelto != 0 && numeroScelto > 6) && contatore<=3) {
+        console.log(contatore);
+        numeroScelto = parseInt(prompt("Riprova ad inserire un numero tra 1 a 5"));//aspetto user input
+        contatore++;
     }
+    if(numeroScelto !=0 && numeroScelto <6){
+        let numeroPc = GeneraNumero(5, 1);//genero il numero
+        let somma = numeroScelto + numeroPc;
+        isNumberEven(somma);//controlla se il numero è paro o disparo
+        if (disparoParoScelta == "pari" && isNumberEven(somma)) {
+            console.log("hai vinto, hai scelto pari (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
+        } else if (disparoParoScelta == "dispari" && !isNumberEven(somma)) {
+            console.log("hai vinto, hai scelto dispari (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
+        } else {
+            console.log("non hai vinto, hai scelto " + disparoParoScelta + " (pc:" + numeroPc + ", tuo:" + numeroScelto + ")");
+        }
+    }else{
+        console.log("Hai inserito un numero troppo grande, ricarica la pagina");
+    }
+    
 }
 //Utiliti--------------------------------------------------------
 function InvertiParola(parolaInserita, parolaInvertita) {
@@ -64,10 +88,10 @@ function InvertiParola(parolaInserita, parolaInvertita) {
     }
     return parolaInvertita;
 }
-function GeneraNumero(max,min){
-    return Math.floor(Math.random() * max +min)
+function GeneraNumero(max, min) {
+    return Math.floor(Math.random() * max + min)
 }
-function isNumberEven(num){//lo scrivo in inglese perchè è intuitivo capire che si tratta di un booleano
-    if(num % 2 == 0) return true; // se è paro ritorna true
+function isNumberEven(num) {//lo scrivo in inglese perchè è intuitivo capire che si tratta di un booleano
+    if (num % 2 == 0) return true; // se è paro ritorna true
     else return false;//se disparo ritorna false
 }
