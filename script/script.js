ca0effe1037308c0c164933f3e1b62d083500fa3
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-05 16:06:44 +0100

    start PariDispari

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
 "use strict";
 console.clear();
 
+
+
+
 /*
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
-
-
-
 PalindromoGioco();
-
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+PariDispari();
 
 
 
@@ -19,6 +27,7 @@ PalindromoGioco();
 
 
 //function 
+//Palindromo
 function PalindromoGioco() {
     //Chiedo all'utente una parola
     const parolaUtente = prompt("Scrivi una parola per verificare se è palindroma!");
@@ -35,6 +44,11 @@ function PalindromoGioco() {
         console.log("non hai inserito una parola");
     }
 }
+//pari dispari
+function PariDispari(){
+    
+}
+//Utiliti
 function InvertiParola(parolaInserita, parolaInvertita) {
     for (let i = parolaInserita.length - 1; i >= 0; i--) {
         parolaInvertita += parolaInserita[i];
